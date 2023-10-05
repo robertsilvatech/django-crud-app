9bbfc6d58f1aaadecb8011d4a151054bd4246ad9
robertsilvatech <contaot@robertsilva.com.br>
2023-10-05 12:26:44 -0300

    add novos testes

--- pastelaria_devops/core/tests/test_view.py
+++ pastelaria_devops/core/tests/test_view.py
@@ -14,3 +14,28 @@ class TestViewProdutosGeral(TestCase):
         data = {'nome': 'Pastel de teste', 'preco': 99.00}
         response = self.client.post(url, data=data)
         self.assertEqual(response.status_code, 302, f"Falha ao criar produto: {response.status_code}")
+
+class TestCRUDProduto(TestCase):
+    def setUp(self):
+        url = reverse('novo_produto')
+        data = {'nome': 'Pastel de teste', 'preco': 99.00}
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, 302, f"Falha ao criar produto: {response.status_code}")        
+
+    def test_crud_editar_produto(self):
+        url = reverse('editar_produto', args=[1])
+        data = {'nome': 'Pastel de teste', 'preco': 10.00 }
+        # Atualiza
+        response = self.client.post(url, data=data)
+        # Consulta preço
+        response = self.client.get(url)
+        self.assertContains(response, 10.00)
+
+    def test_crud_deleta_produto(self):
+        url = reverse('deletar_produto', args=[1])
+        # deleta
+        response = self.client.post(url)
+        # consulta
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
